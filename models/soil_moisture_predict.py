edf8b98b33b75d5d8f5141f5f82652e2a37abde5
AKai <2780896955@qq.com>
2024-04-15 12:07:57 +0800

    vgg16换用resnet18

--- models/soil_moisture_predict.py
+++ models/soil_moisture_predict.py
@@ -1,31 +1,63 @@
 from models.vgg16 import VGG16
 from models.mlp import MLP
 from models.encoder import TransformerEncoder
+from models.resnet import ResNet, BasicBlock
 from utils.config_manager import ConfigManager
 from pathlib import Path
 import torch.nn as nn
 import torch
 import timm
+from torchvision import transforms
 config = ConfigManager()
 VGG_WEIGHTS_PATH = str(Path(__file__).parent.parent / 'model_weights/vgg16.bin')
+RESNET18_WEIGHTS_PATH = str(Path(__file__).parent.parent / 'model_weights/resnet18.bin')
 class RGB_Model(nn.Module):
     def __init__(self):
         super(RGB_Model, self).__init__()
         soil_water_conf = config.get('soil_water_predict_model')['rgb']
-        self.rgb_vgg16 = VGG16(VGG_WEIGHTS_PATH, num_classes=soil_water_conf['rgb_vgg16']['output_dim'], finetune=soil_water_conf['rgb_vgg16']['finetune'])
-        self.data_config = timm.data.resolve_model_data_config(self.rgb_vgg16.model)
-        self.mlp = MLP(
+        # self.rgb_vgg16 = VGG16(VGG_WEIGHTS_PATH, num_classes=soil_water_conf['rgb_vgg16']['output_dim'], finetune=soil_water_conf['rgb_vgg16']['finetune'])
+        # self.data_config = timm.data.resolve_model_data_config(self.rgb_vgg16.model)
+        self.rgb_resnet18 = ResNet(
+            block=BasicBlock,
+            layers=[2,2,2,2],
+            num_classes=soil_water_conf['rgb_resnet18']['output_dim']
+            # num_classes=1000
+        )
+        # self.rgb_resnet18.fc = nn.Linear(self.rgb_resnet18.fc.in_features, soil_water_conf['rgb_resnet18']['output_dim'])
+        if soil_water_conf['rgb_resnet18']['finetune']:
+            state_dict = torch.load(RESNET18_WEIGHTS_PATH)
+            state_dict.pop('fc.weight', None)
+            state_dict.pop('fc.bias', None)
+            self.rgb_resnet18.load_state_dict(state_dict, strict=False)
+        self.final_mlp = MLP(
             input_size=soil_water_conf['mlp']['input_dim'], 
             hidden_size=soil_water_conf['mlp']['hidden_dim'], 
             num_classes=soil_water_conf['mlp']['output_dim']
         )
 
     def get_image_transform(self, is_training=False):
-        return timm.data.create_transform(**self.data_config, is_training=is_training)
+        # return timm.data.create_transform(**self.data_config, is_training=is_training)
+        if is_training:
+            _transform = transforms.Compose([
+                transforms.Resize(256),
+                transforms.RandomCrop(224),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+            ])
+        else:
+            _transform = transforms.Compose([
+                transforms.Resize(256),            # 将图像大小调整一致
+                transforms.CenterCrop(224),        # 中心裁剪以匹配模型输入
+                transforms.ToTensor(),             # 转换为张量
+                transforms.Normalize(mean=[0.485, 0.456, 0.406],  # 标准化处理
+                                    std=[0.229, 0.224, 0.225])
+            ])
+        return _transform  
 
     def forward(self, rgb_image):
-        image_embd = self.rgb_vgg16(rgb_image)
-        return self.mlp(image_embd)
+        image_embd = self.rgb_resnet18(rgb_image)
+        return self.final_mlp(image_embd)
 
 class RGB_TM_Model(nn.Module):
     def __init__(self):
@@ -47,8 +79,8 @@ class RGB_TM_Model(nn.Module):
             num_classes=soil_water_conf['mlp']['output_dim']
         )
 
-    def get_image_transform(self, is_training=False):
-        return timm.data.create_transform(**self.data_config, is_training=is_training)
+    # def get_image_transform(self, is_training=False):
+    #     return timm.data.create_transform(**self.data_config, is_training=is_training)
 
     def forward(self, rgb_image, T_moisture):
         image_embd = self.rgb_vgg16(rgb_image)
