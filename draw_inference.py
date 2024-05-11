7add8e64dfa3ab1518e5ca0c026a27f1657121fd
AKai <2780896955@qq.com>
2024-05-11 09:47:41 +0800

    draw

--- draw_inference.py
+++ draw_inference.py
@@ -4,6 +4,7 @@ import dataset.sapflow_predict as sfp_datasets
 import dataset.soil_moisture_predict as smp_datasets
 import torch
 from pathlib import Path
+import numpy as np
 from utils.inference import compare_models, read_labels, draw_infer_graph, cal_r2
 from torch.utils.data import DataLoader
 def infer_and_draw():
@@ -11,10 +12,17 @@ def infer_and_draw():
     batch_size = 32
     num_workers = 12
     src_dir = Path(__file__).resolve().parent
-    model1 = sfp_models.RgbVgg16TmModel()
-    model1.load_state_dict(torch.load(src_dir/'train/sapflow_predict/rgb_vgg16_tm/best2/best_weights.pth', map_location=device))
-    model2 = sfp_models.RgbResNet18TmModel()
-    model2.load_state_dict(torch.load(src_dir/'train/sapflow_predict/rgb_resnet18_tm/best1/best_weights.pth', map_location=device))
+    # -----需要改------
+    model1 = smp_models.RgbVgg16Model()
+    model1.load_state_dict(torch.load(src_dir/'train/soil_moisture_predict/segment_rgb_vgg16/14r/best_weights.pth', map_location=device))
+    # model1 = sfp_models.RgbResNet18Model()
+    # model1.load_state_dict(torch.load(src_dir/'train/sapflow_predict/rgb_resnet18/best1/best_weights.pth', map_location=device))
+    model2 = model1
+    # model2 = smp_models.RgbVgg16Model()
+    # model2.load_state_dict(torch.load(src_dir/'train/soil_moisture_predict/segment_rgb_vgg16/14r/best_weights.pth', map_location=device))
+    # model2 = sfp_models.RgbResNet18TmModel()
+    # model2.load_state_dict(torch.load(src_dir/'train/sapflow_predict/rgb_resnet18_tm/best1/best_weights.pth', map_location=device))
+    # -----需要改------
 
     rgb_images_dir = src_dir / 'data' / 'rgb_images'
     segment_images_dir = src_dir / 'data' / 'segment_rgb_images'
@@ -24,38 +32,73 @@ def infer_and_draw():
     sapflow_rgb_images_dir = src_dir / 'data' / 'sapflow_predict_data' / 'rgb_images'
     sapflow_dir = src_dir / 'data' / 'sapflow_predict_data' / 'sapflow'
 
-    dataset = sfp_datasets.RgbTmDataset(
-        rgb_images_dir=sapflow_rgb_images_dir,
-        tm_file_path=temp_moisture_file_path,
-        sapflow_dir=sapflow_dir,
+    # -----需要改------
+    # dataset = sfp_datasets.RgbTmDataset(
+    #     rgb_images_dir=sapflow_rgb_images_dir,
+    #     tm_file_path=temp_moisture_file_path,
+    #     sapflow_dir=sapflow_dir,
+    #     transform=model1.get_image_transform(is_training=False)
+    # )
+    dataset = smp_datasets.rgb_tm_dataset(
+        # rgb_images_dir=rgb_images_dir,
+        rgb_images_dir=segment_images_dir,
+        temp_moisture_filepath=temp_moisture_file_path,
+        soil_moisture_filepath=soil_moisture_file_path,
         transform=model1.get_image_transform(is_training=False)
     )
+    # -----需要改------
+
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    output_dir = Path(src_dir/'graphs/true_vs_predict/sapflow')
+
+    # -----需要改------
+    # output_dir = Path(src_dir/'graphs/true_vs_predict/sapflow')
+    output_dir = Path(src_dir/'graphs/true_vs_predict/soil_moisture')
+    # -----需要改------
+
     output_dir.mkdir(parents=True, exist_ok=True)
+
+    # -----需要改------
     compare_models(
         model1=model1, model1_use_tm=False,
         model2=model2, model2_use_tm=False,
         dataloader=dataloader,
-        output_file=output_dir / 'rgb_tm_vgg16_vs_resnet18.png',
-        y_label='Stem Flow (g/h)',
+        soil_moisture=True,
+        output_file=output_dir / 'segment_rgb_vgg16.png',
+        # y_label='True Stem Flow (g/h)',
+        # x_label='Predicted Stem Flow (g/h)',
+        y_label='True Soil Moisture (%)',
+        x_label='Predicted Soil Moisture (%)',
         device=device,
-        label1='vgg16_tm',
-        label2='resnet18_tm'
+        legend_label1='VGG16',
+        legend_label2='VGG16'
     )
+    # -----需要改------
+    
   
 
 def draw():
     src_dir = Path(__file__).resolve().parent
-    labels = read_labels(label_file=src_dir/'graphs/true_vs_predict/sapflow/rgb_tm_vgg16_vs_resnet18.txt')
-    labels = labels
+    # -----需要改------
+    # labels = read_labels(label_file=src_dir/'graphs/true_vs_predict/sapflow/rgb_vgg16_vs_resnet18.txt')
+    labels = read_labels(label_file=src_dir/'graphs/true_vs_predict/soil_moisture/only_rgb_resnet18_vs_rgb_tm_resnet18.txt')
+    # -----需要改------
+    labels = np.array(labels)
+    labels[:, 2] = labels[:, 2] + 1
+    # labels[:, [1,2]] = labels[:, [2,1]]
+
+    # -----需要改------
     draw_infer_graph(
         labels=labels, 
-        output_file=src_dir/'graphs/true_vs_predict/sapflow/rgb_vgg16_vs_resnet18.png', 
-        y_label='Stem Flow (g/h)',
-        label1='vgg16',
-        label2='resnet18'
+        # output_file=src_dir/'graphs/true_vs_predict/sapflow/rgb_vgg16_vs_resnet18.png', 
+        output_file=src_dir/'graphs/true_vs_predict/soil_moisture/only_rgb_resnet18_vs_rgb_tm_resnet18.png', 
+        # y_label='True Stem Flow (g/h)',
+        # x_label='Predicted Stem Flow (g/h)',
+        y_label='True Soil Moisture (%)',
+        x_label='Predicted Soil Moisture (%)',
+        label1='ResNet18',
+        label2='ResNet18_tm'
     )
+    # -----需要改------
     cal_r2(labels)
     
 
@@ -63,3 +106,4 @@ def draw():
 
 if __name__ == '__main__':
     infer_and_draw()
+    # draw()
--- utils/inference.py
+++ utils/inference.py
@@ -6,15 +6,20 @@ import torch.nn as nn
 from pathlib import Path
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
+import numpy as np
 def compare_models(
         model1: nn.Module, model1_use_tm: bool,
         model2: nn.Module, model2_use_tm: bool,
+        # because soil moisture rgb and tm dataset will return 4 values, 
+        # the last one is deprecated but still not removed
+        soil_moisture: bool,
         dataloader: torch.utils.data.DataLoader, 
         output_file: Path, 
         device: torch.device,
-        label1: str,
-        label2: str,
-        y_label: str
+        legend_label1: str,
+        legend_label2: str,
+        y_label: str,
+        x_label: str
     ):
     model1.eval()
     model2.eval()
@@ -22,7 +27,11 @@ def compare_models(
     model2.to(device)
     labels = []
     count = 0
-    for rgb_image, temp_moistures, label in dataloader:
+    for data in dataloader:
+        if soil_moisture:
+            rgb_image, temp_moistures, label, _ = data
+        else:
+            rgb_image, temp_moistures, label = data
         rgb_image = rgb_image.to(device)
         temp_moistures = temp_moistures.to(device)
         label = label.to(device)
@@ -46,7 +55,14 @@ def compare_models(
     with open(output_file.parent/(output_file.stem+'.txt'), 'w') as f:
         for label, label1, label2 in labels:
             f.write(f'{label} {label1} {label2}\n')
-    draw_infer_graph(labels=labels, output_file=output_file, y_label=y_label, label1=label1, label2=label2)
+    draw_infer_graph(
+        labels=labels, 
+        output_file=output_file, 
+        x_label=x_label, 
+        y_label=y_label, 
+        label1=legend_label1, 
+        label2=legend_label2
+    )
     # fig, ax = plt.subplots()
     # xs = range(len(labels))
     # ax.spines['top'].set_color('none')
@@ -60,6 +76,7 @@ def compare_models(
     # ax.set_ylabel(y_label)
     # ax.legend()
     # plt.savefig(output_file)
+    cal_r2(labels)
     
 
 def read_labels(label_file: Path)->list[Tuple[float, float, float]]:
@@ -69,19 +86,31 @@ def read_labels(label_file: Path)->list[Tuple[float, float, float]]:
             labels.append(list(map(float, line.strip().split(' '))))
     return labels
 
-def draw_infer_graph(labels: list[Tuple[float, float, float]], output_file:Path, y_label: str, label1:str, label2:str):
-    fig, ax = plt.subplots(figsize=(10,6))
+def draw_infer_graph(labels: list[Tuple[float, float, float]], output_file:Path,x_label:str, y_label: str, label1:str, label2:str):
+    fig, ax = plt.subplots(figsize=(10,10))
     xs = range(len(labels))
+    true_labels = np.array([label[0] for label in labels])
+    pred1_labels = np.array([label[1] for label in labels])
+    pred2_labels = np.array([label[2] for label in labels])
+    k1,b1 = np.polyfit(pred1_labels, true_labels, 1)
+    k2,b2 = np.polyfit(pred2_labels, true_labels, 1)
+
     # ax.spines['top'].set_color('none')
     # ax.spines['bottom'].set_color('none')
     # ax.spines['left'].set_color('none')
-    ax.plot(xs, [label[0] for label in labels], label='true', color='dimgray', linestyle='solid', linewidth=2)
+    # ax.plot(xs, [label[0] for label in labels], label='true', color='dimgray', linestyle='solid', linewidth=2)
     # ax.plot(xs, [label[1] for label in labels], label=label1, color='blue', linestyle='solid', linewidth=2)
     # ax.plot(xs, [label[2] for label in labels], label=label2, color='red', linestyle='solid', linewidth=2)
     # ax.scatter(xs, [label[1] for label in labels], label=label1, color='black', marker='o')
-    ax.scatter(xs, [label[1] for label in labels], label=label1, color='deepskyblue', marker='x')
-    ax.scatter(xs, [label[2] for label in labels], label=label2, color='orange', marker='o')
-    # ax.set_xlabel('Epoch')
+    ax.scatter(pred1_labels, true_labels, label=label1, color='deepskyblue', marker='x')
+    ax.plot(pred1_labels, k1*pred1_labels+b1, color='deepskyblue', linestyle='solid', linewidth=3)
+    ax.scatter(pred2_labels, true_labels, label=label2, color='orange', marker='o')
+    ax.plot(pred2_labels, k2*pred2_labels+b2, color='orange', linestyle='solid', linewidth=3)
+
+    x_min, x_max = ax.get_xlim()
+    xs = np.linspace(x_min, x_max, 100)
+    ax.plot(xs, xs, color='lightgray', linestyle='solid')
+    ax.set_xlabel(x_label)
     ax.set_ylabel(y_label)
     ax.legend()
     plt.savefig(output_file)
